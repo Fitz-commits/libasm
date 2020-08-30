bdb24ded261cbf061ba7f1640d1b5b95600cedfb
Fitz-commits <cdelaby@student.s19.be>
2020-08-30 23:36:49 +0200

    It is working but a lot to do to make it work

--- ft_read.s
+++ ft_read.s
@@ -1,22 +1,24 @@
+extern ___error
 global  _ft_read
 ; read(int fildes rdi, void *buf rsi, size_t nbyte rdx);
 _ft_read:
-    cmp rdi, 0
-    jl FT_FD_LESS_0
-    cmp rdx, 0
-    je FT_NOSIZE
-    cmp rsi, 0
-    je FT_NO_STRING
+    push rdi
+    call ___error
+    cmp dword [rax], 0
+    jne _continue
+    mov dword [rax], 0
+    _continue:
+    pop rdi
     mov rax, 0x2000003
     syscall
-    jc FT_FD_LESS_0
+    jc _error
     ret
-FT_NOSIZE:
-    mov rax, 0
-    ret
-FT_FD_LESS_0:
+_error :
+    push r10
+    mov r10, rax
+    call ___error
+    mov [rax], r10
+    neg rax
     mov rax, -1
-    ret
-FT_NO_STRING:
-    mov rax, -1
+    pop r10
     ret
